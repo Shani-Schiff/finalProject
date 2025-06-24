a199af41ce38770b9e5983f4f7cbc628c3c985f0
Miri0442 <miricitron704@gmail.com>
2025-06-24 12:21:44 +0300

    .

--- server/models/User.js
+++ server/models/User.js
@@ -2,23 +2,24 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../../dataBase/dataBase');
 
 const User = sequelize.define('User', {
-  user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  user_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true, field: 'user_id' },
   user_name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   phone_number: { type: DataTypes.STRING, allowNull: true },
+  role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'student' },
   active_status: { type: DataTypes.BOOLEAN, defaultValue: true },
-  created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  date_created: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
 }, {
   tableName: 'users',
-  underscored: true,
-  timestamps: false
+  timestamps: true
 });
 
-User.associate = models => {
-  User.hasOne(models.user_password, { foreignKey: 'user_id' });
-  User.belongsToMany(models.role, { through: models.user_role, foreignKey: 'user_id' });
-  User.hasMany(models.lesson, { foreignKey: 'teacher_id', as: 'teaching_lessons' });
-  User.belongsToMany(models.lesson, { through: models.lesson_student, foreignKey: 'user_id', as: 'student_lessons' });
+
+User.associate = (models) => {
+  User.hasOne(models.UserPassword, { foreignKey: 'user_id' });
+  User.hasMany(models.Lesson, { foreignKey: 'teacher_id', as: 'teachingLessons' });
+  User.belongsToMany(models.Lesson, { through: models.LessonStudent, foreignKey: 'user_id', as: 'studentLessons' });
+  User.belongsToMany(models.Lesson, { through: models.LessonTeacher, foreignKey: 'user_id', as: 'teacherLessons' });
 };
 
 module.exports = User;
